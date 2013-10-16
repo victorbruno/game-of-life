37b4f439645199346e0bddd5772b81b06dd66826
Victor <victorbrunoparente@gmail.com>
2013-10-16 15:53:08 -0300

    alterado de asterisco para cruz

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,8 +1,9 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-    LIVE_CELL("*"), DEAD_CELL(".");
-
+    //LIVE_CELL("*"), DEAD_CELL(".");
+	LIVE_CELL("+"), DEAD_CELL(".");
+	
     private String symbol;
 
     private Cell(final String initialSymbol) {
